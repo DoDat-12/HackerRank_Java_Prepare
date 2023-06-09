0fba760e094ccd8daed94aba8e183947904be229
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-06-09 15:31:50 +0700

    06/09/2023

---
+++ HackerRank_Java_Prepare/src/Java_If_Else/Solution.java
@@ -0,0 +1,11 @@
+package Java_If_Else;
+import java.util.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+    }
+}
