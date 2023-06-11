454c72e91c1d1ca642436efac03d9cdd980450c9
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-06-11 09:44:58 +0700

    11/06/2023

---
+++ HackerRank_Java_Prepare/src/Java_Static_Initializer_Block/Solution.java
@@ -0,0 +1,26 @@
+package Java_Static_Initializer_Block;
+import java.util.*;
+
+public class Solution {
+
+    //Write your code here
+    public static int B;
+    public static int H;
+    public static boolean flag = true;
+    static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }//end of main
+}//end of class
+
